d825d5d5d471d72c0a472f52b2bb8ff8e7939cc4
Patrick Zhang <zhangpatrick2004@gmail.com>
2025-07-25 18:15:24 -0700

    basic web scaffolding (changeable)

---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx'
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Root element not found");
+}
